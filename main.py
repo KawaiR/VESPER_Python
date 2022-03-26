e96fdb56150006b5283f05c3a4f26c956e0dfd3c
AhaiBeast <zhuhan568@outlook.com>
2022-03-25 21:47:26 -0400

    Rollback and fixing argument passing problem

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
+import argparse
 import concurrent.futures
 import copy
-import math
-import multiprocessing
-import os
 import csv
+import math
 
-import argparse
-from typing import Text
-#import mkdir
 import mrcfile
+import multiprocessing
 import numba
 import numpy as np
+import os
 import pyfftw
 import scipy.fft
-from numba.typed import List
-from scipy.ndimage import convolve
-from scipy.spatial.transform import Rotation as R
-#from tqdm.notebook import tqdm
-
 from VESPER_1_prob_new import *
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -33,12 +25,17 @@ if __name__ == "__main__":
     orig.add_argument('-b', type=str, required=True, help='MAP2.mrc (small)')
     orig.add_argument('-t', type=float, help='Threshold of density map1')
     orig.add_argument('-T', type=float, help='Threshold of density map2')
-    orig.add_argument('-g', type=float, default=16.0, help='Bandwidth of the Gaussian filter def=16.0, sigma = 0.5*[value entered]')
+    orig.add_argument('-g', type=float, default=16.0,
+                      help='Bandwidth of the Gaussian filter def=16.0, sigma = 0.5*[value entered]')
     orig.add_argument('-s', type=float, default=7.0, help='Sampling voxel spacing def=7.0')
     orig.add_argument('-A', type=float, default=30.0, help='Sampling angle spacing def=30.0')
     orig.add_argument('-N', type=int, default=10, help='Refine Top [int] models def=10')
     orig.add_argument('-S', type=bool, default=False, help='Show topN models in PDB format def=false')
-    orig.add_argument('-M', type=str, default='V', help='V: vector product mode (default)\nO: overlap mode\nC: Cross Correlation Coefficient Mode\nP: Pearson Correlation Coefficient Mode\nL: Laplacian Filtering Mode')
+    orig.add_argument('-M', type=str, default='V', help='V: vector product mode (default)\n' +
+                                                        'O: overlap mode\n' +
+                                                        'C: Cross Correlation Coefficient Mode\n' +
+                                                        'P: Pearson Correlation Coefficient Mode\n' +
+                                                        'L: Laplacian Filtering Mode')
     orig.add_argument('-E', type=bool, default=False, help='Evaluation mode of the current position def=false')
 
     # secondary structure matching menu
@@ -46,55 +43,60 @@ if __name__ == "__main__":
     prob.add_argument('-npa', type=str, required=True, help='numpy array for Predictions for map 1')
     prob.add_argument('-b', type=str, required=True, help='MAP2.mrc (small)')
     prob.add_argument('-npb', type=str, required=True, help='numpy array for Predictions for map 2')
-    prob.add_argument('-alpha', type=int, default=1,required=False, help='The weighting parameter ')
+    prob.add_argument('-alpha', type=int, default=1, required=False, help='The weighting parameter ')
     prob.add_argument('-t', type=float, help='Threshold of density map1')
     prob.add_argument('-T', type=float, help='Threshold of density map2')
-    prob.add_argument('-g', type=float, default=16.0, help='Bandwidth of the Gaussian filter def=16.0, sigma = 0.5*[value entered]')
+    prob.add_argument('-g', type=float, default=16.0,
+                      help='Bandwidth of the Gaussian filter def=16.0, sigma = 0.5*[value entered]')
     prob.add_argument('-s', type=float, default=7.0, help='Sampling voxel spacing def=7.0')
     prob.add_argument('-A', type=float, default=30.0, help='Sampling angle spacing def=30.0')
     prob.add_argument('-N', type=int, default=10, help='Refine Top [int] models def=10')
     prob.add_argument('-S', type=bool, default=False, help='Show topN models in PDB format def=false')
-    prob.add_argument('-M', type=str, default='V', help='V: vector product mode (default)\nO: overlap mode\nC: Cross Correlation Coefficient Mode\nP: Pearson Correlation Coefficient Mode\nL: Laplacian Filtering Mode')
+    prob.add_argument('-M', type=str, default='V', help='V: vector product mode (default)\n' +
+                                                        'O: overlap mode\n' +
+                                                        'C: Cross Correlation Coefficient Mode\n' +
+                                                        'P: Pearson Correlation Coefficient Mode\n' +
+                                                        'L: Laplacian Filtering Mode')
     prob.add_argument('-E', type=bool, default=False, help='Evaluation mode of the current position def=false')
-
 
     args = parser.parse_args()
 
     if args.command == 'orig':
-        #mrc file paths
+        # mrc file paths
         objA = args.a
         objB = args.b
 
-        #all optional positional arguments
-        #check for none
+        # all optional positional arguments
+        # check for none
         threshold1 = args.t
         threshold2 = args.T
 
         if threshold1 is None:
-            threshold1 = 0.01
-        
+            threshold1 = 0.00
+
         if threshold2 is None:
-            threshold2 = 0.01
-        
-        #with default
+            threshold2 = 0.00
+
+        # with default
         bandwidth = args.g
         voxel_spacing = args.s
         angle_spacing = args.A
         topN = args.N
 
-        #necessary?
+        # necessary?
         showPdb = args.S
-        
+
         modeVal = args.M
         evalMode = args.E
 
-        print(objA, objB, threshold1, threshold2, bandwidth, voxel_spacing, angle_spacing, topN, showPdb, modeVal, evalMode)
-        
-        #construct mrc objects
+        print(objA, objB, threshold1, threshold2, bandwidth, voxel_spacing, angle_spacing, topN, showPdb, modeVal,
+              evalMode)
+
+        # construct mrc objects
         mrc1 = mrc_obj(objA)
         mrc2 = mrc_obj(objB)
 
-        #set voxel size
+        # set voxel size
         mrc1, mrc_N1 = mrc_set_vox_size(mrc1, threshold1, voxel_spacing)
         mrc2, mrc_N2 = mrc_set_vox_size(mrc2, threshold2, voxel_spacing)
 
@@ -119,70 +121,71 @@ if __name__ == "__main__":
         mrc_N2.vec = np.zeros((dim, dim, dim, 3), dtype="float32")
         mrc_N2.data = np.zeros((dim, dim, dim))
 
-        #fastVEC
+        # fastVEC
         mrc_N1 = fastVEC(mrc1, mrc_N1, bandwidth)
         mrc_N2 = fastVEC(mrc2, mrc_N2, bandwidth)
 
-        #search map
+        # search map
         if modeVal == 'V':
-            modeVal="VecProduct"
+            modeVal = "VecProduct"
         elif modeVal == 'O':
-            modeVal="Overlap"
+            modeVal = "Overlap"
         elif modeVal == 'C':
-            modeVal="CC"
+            modeVal = "CC"
         elif modeVal == 'P':
-            modeVal="PCC"
+            modeVal = "PCC"
         elif modeVal == 'L':
-            modeVal="Laplacian"
-        
+            modeVal = "Laplacian"
+
         search_map_fft(mrc_N1, mrc_N2, TopN=topN, ang=angle_spacing, mode=modeVal, is_eval_mode=evalMode)
-        
+
 
     elif args.command == 'prob':
-        #mrc file paths
+        # mrc file paths
         objA = args.a
         objB = args.b
 
-        #numpy arrays 
+        # numpy arrays
 
-        npA=args.npa
-        npB=args.npb
+        npA = args.npa
+        npB = args.npb
 
-        #probability map file paths
+        # probability map file paths
         probA = args.a
         probB = args.b
 
-        #all optional positional arguments
-        #check for none
+        # all optional positional arguments
+        # check for none
         threshold1 = args.t
         threshold2 = args.T
 
-        #weighting parameter
-        alpha=args.alpha
+        # weighting parameter
+        alpha = args.alpha
 
         if threshold1 is None:
-            threshold1 = 0.01
-        
+            threshold1 = 0.00
+
         if threshold2 is None:
-            threshold2 = 0.01
-        
-        #with default
+            threshold2 = 0.00
+
+        # with default
         bandwidth = args.g
         voxel_spacing = args.s
         angle_spacing = args.A
         topN = args.N
 
-        #necessary?
+        # necessary?
         showPdb = args.S
-        
+
         modeVal = args.M
         evalMode = args.E
 
-        print(objA, objB, probA, probB, threshold1, threshold2, bandwidth, voxel_spacing, angle_spacing, topN, showPdb, modeVal, evalMode)
+        print(objA, objB, probA, probB, threshold1, threshold2, bandwidth, voxel_spacing, angle_spacing, topN, showPdb,
+              modeVal, evalMode)
 
-        prob_maps=np.load(npA)
-        prob_maps_chain=np.load(npB)
-        
+        prob_maps = np.load(npA)
+        prob_maps_chain = np.load(npB)
+
         mrc1 = mrc_obj(objA)
         mrc2 = mrc_obj(objB)
         print(mrc2.xdim)
@@ -190,7 +193,7 @@ if __name__ == "__main__":
         print(mrc2.zdim)
         print(mrc2.data.shape)
 
-        #probability MRCs
+        # probability MRCs
 
         mrc1_p1 = mrc_obj(objA)
         mrc1_p2 = mrc_obj(objA)
@@ -201,65 +204,33 @@ if __name__ == "__main__":
         mrc2_p2 = mrc_obj(objB)
         mrc2_p3 = mrc_obj(objB)
         mrc2_p4 = mrc_obj(objB)
-        #
-        #
-        ''''mrc2_p1.xdim=prob_maps_chain.shape[0]
-        mrc2_p1.ydim=prob_maps_chain.shape[1]
-        mrc2_p1.zdim=prob_maps_chain.shape[2]
 
-        mrc2_p2.xdim=prob_maps_chain.shape[0]
-        mrc2_p2.ydim=prob_maps_chain.shape[1]
-        mrc2_p2.zdim=prob_maps_chain.shape[2]
-
-        mrc2_p3.xdim=prob_maps_chain.shape[0]
-        mrc2_p3.ydim=prob_maps_chain.shape[1]
-        mrc2_p3.zdim=prob_maps_chain.shape[2]
-
-        mrc2_p4.xdim=prob_maps_chain.shape[0]
-        mrc2_p4.ydim=prob_maps_chain.shape[1]
-        mrc2_p4.zdim=prob_maps_chain.shape[2]'''
-
-        mrc2_p1.data=np.swapaxes(prob_maps_chain[:,:,:,0], 0, 2)
+        mrc2_p1.data = np.swapaxes(prob_maps_chain[:, :, :, 0], 0, 2)
         print(mrc2_p1.data.shape)
-        mrc2_p2.data=np.swapaxes(prob_maps_chain[:,:,:,1], 0, 2)
-        mrc2_p3.data=np.swapaxes(prob_maps_chain[:,:,:,2], 0, 2)
-        mrc2_p4.data=np.swapaxes(prob_maps_chain[:,:,:,3], 0, 2)
+        mrc2_p2.data = np.swapaxes(prob_maps_chain[:, :, :, 1], 0, 2)
+        mrc2_p3.data = np.swapaxes(prob_maps_chain[:, :, :, 2], 0, 2)
+        mrc2_p4.data = np.swapaxes(prob_maps_chain[:, :, :, 3], 0, 2)
 
-        ''''mrc1_p1.xdim=prob_maps.shape[0]
-        mrc1_p1.ydim=prob_maps.shape[1]
-        mrc1_p1.zdim=prob_maps.shape[2]
+        mrc1_p1.data = np.swapaxes(prob_maps[:, :, :, 0], 0, 2)
+        mrc1_p2.data = np.swapaxes(prob_maps[:, :, :, 1], 0, 2)
+        mrc1_p3.data = np.swapaxes(prob_maps[:, :, :, 2], 0, 2)
+        mrc1_p4.data = np.swapaxes(prob_maps[:, :, :, 3], 0, 2)
 
-        mrc1_p2.xdim=prob_maps.shape[0]
-        mrc1_p2.ydim=prob_maps.shape[1]
-        mrc1_p2.zdim=prob_maps.shape[2]
+        mrc1, mrc_N1 = mrc_set_vox_size(mrc1, threshold1, voxel_spacing)
+        mrc2, mrc_N2 = mrc_set_vox_size(mrc2, threshold2, voxel_spacing)
+        mrc1_p1, mrc_N1_p1 = mrc_set_vox_size(mrc1_p1, th=0.0, voxel_size=voxel_spacing)
+        mrc1_p2, mrc_N1_p2 = mrc_set_vox_size(mrc1_p2, th=0.0, voxel_size=voxel_spacing)
+        mrc1_p3, mrc_N1_p3 = mrc_set_vox_size(mrc1_p3, th=0.0, voxel_size=voxel_spacing)
+        mrc1_p4, mrc_N1_p4 = mrc_set_vox_size(mrc1_p4, th=0.0, voxel_size=voxel_spacing)
+        mrc2_p1, mrc_N2_p1 = mrc_set_vox_size(mrc2_p1, th=0.0, voxel_size=voxel_spacing)
+        mrc2_p2, mrc_N2_p2 = mrc_set_vox_size(mrc2_p2, th=0.0, voxel_size=voxel_spacing)
+        mrc2_p3, mrc_N2_p3 = mrc_set_vox_size(mrc2_p3, th=0.0, voxel_size=voxel_spacing)
+        mrc2_p4, mrc_N2_p4 = mrc_set_vox_size(mrc2_p4, th=0.0, voxel_size=voxel_spacing)
 
-        mrc1_p3.xdim=prob_maps.shape[0]
-        mrc1_p3.ydim=prob_maps.shape[1]
-        mrc1_p3.zdim=prob_maps.shape[2]
-
-        mrc1_p4.xdim=prob_maps.shape[0]
-        mrc1_p4.ydim=prob_maps.shape[1]
-        mrc1_p4.zdim=prob_maps.shape[2]'''
-
-        mrc1_p1.data=np.swapaxes(prob_maps[:,:,:,0], 0, 2)
-        mrc1_p2.data=np.swapaxes(prob_maps[:,:,:,1], 0, 2)
-        mrc1_p3.data=np.swapaxes(prob_maps[:,:,:,2], 0, 2)
-        mrc1_p4.data=np.swapaxes(prob_maps[:,:,:,3], 0, 2)
-
-
-        mrc1, mrc_N1 = mrc_set_vox_size(mrc1)
-        mrc2, mrc_N2 = mrc_set_vox_size(mrc2)
-        mrc1_p1, mrc_N1_p1 = mrc_set_vox_size(mrc1_p1)
-        mrc1_p2, mrc_N1_p2 = mrc_set_vox_size(mrc1_p2)
-        mrc1_p3, mrc_N1_p3 = mrc_set_vox_size(mrc1_p3)
-        mrc1_p4, mrc_N1_p4 = mrc_set_vox_size(mrc1_p4)
-        mrc2_p1, mrc_N2_p1 = mrc_set_vox_size(mrc2_p1)
-        mrc2_p2, mrc_N2_p2 = mrc_set_vox_size(mrc2_p2)
-        mrc2_p3, mrc_N2_p3 = mrc_set_vox_size(mrc2_p3)
-        mrc2_p4, mrc_N2_p4 = mrc_set_vox_size(mrc2_p4)
-
-        mrc_list = [mrc_N1, mrc_N2, mrc_N1_p1, mrc_N1_p2, mrc_N1_p3, mrc_N1_p4, mrc_N2_p1, mrc_N2_p2, mrc_N2_p3, mrc_N2_p4]
-        max_dim = np.max((mrc_N1.xdim, mrc_N2.xdim, mrc_N2_p1.xdim, mrc_N2_p2.xdim, mrc_N2_p3.xdim, mrc_N2_p4.xdim, mrc_N1_p1.xdim, mrc_N1_p2.xdim, mrc_N1_p3.xdim, mrc_N1_p4.xdim))
+        mrc_list = [mrc_N1, mrc_N2, mrc_N1_p1, mrc_N1_p2, mrc_N1_p3, mrc_N1_p4, mrc_N2_p1, mrc_N2_p2, mrc_N2_p3,
+                    mrc_N2_p4]
+        max_dim = np.max((mrc_N1.xdim, mrc_N2.xdim, mrc_N2_p1.xdim, mrc_N2_p2.xdim, mrc_N2_p3.xdim, mrc_N2_p4.xdim,
+                          mrc_N1_p1.xdim, mrc_N1_p2.xdim, mrc_N1_p3.xdim, mrc_N1_p4.xdim))
 
         print(max_dim)
 
@@ -275,33 +246,29 @@ if __name__ == "__main__":
             mrc.data = np.zeros((max_dim, max_dim, max_dim), dtype="float32")
             mrc.vec = np.zeros((max_dim, max_dim, max_dim, 3), dtype="float32")
 
-            #search map
+            # search map
             if modeVal == 'V':
-                modeVal="vecProduct"
+                modeVal = "vecProduct"
             elif modeVal == 'O':
-                modeVal="Overlap"
+                modeVal = "Overlap"
             elif modeVal == 'C':
-                modeVal="CC"
+                modeVal = "CC"
             elif modeVal == 'P':
-                modeVal="PCC"
+                modeVal = "PCC"
             elif modeVal == 'L':
-                modeVal="Laplacian"
+                modeVal = "Laplacian"
 
-        #THIS EXECUTION TAKES TOO MUCH TIME
-        #%%time
-        mrc_N1 = fastVEC(mrc1, mrc_N1)
-        mrc_N1_p1 = fastVEC_prob(mrc1_p1, mrc_N1_p1)
-        mrc_N1_p2 = fastVEC_prob(mrc1_p2, mrc_N1_p2)
-        mrc_N1_p3 = fastVEC_prob(mrc1_p3, mrc_N1_p3)
-        mrc_N1_p4 = fastVEC_prob(mrc1_p4, mrc_N1_p4)
+        mrc_N1 = fastVEC(mrc1, mrc_N1, dreso=bandwidth)
+        mrc_N1_p1 = fastVEC(mrc1_p1, mrc_N1_p1, dreso=bandwidth)
+        mrc_N1_p2 = fastVEC(mrc1_p2, mrc_N1_p2, dreso=bandwidth)
+        mrc_N1_p3 = fastVEC(mrc1_p3, mrc_N1_p3, dreso=bandwidth)
+        mrc_N1_p4 = fastVEC(mrc1_p4, mrc_N1_p4, dreso=bandwidth)
 
+        mrc_N2 = fastVEC(mrc2, mrc_N2, dreso=bandwidth)
+        mrc_N2_p1 = fastVEC(mrc2_p1, mrc_N2_p1, dreso=bandwidth)
+        mrc_N2_p2 = fastVEC(mrc2_p2, mrc_N2_p2, dreso=bandwidth)
+        mrc_N2_p3 = fastVEC(mrc2_p3, mrc_N2_p3, dreso=bandwidth)
+        mrc_N2_p4 = fastVEC(mrc2_p4, mrc_N2_p4, dreso=bandwidth)
 
-
-        mrc_N2 = fastVEC(mrc2, mrc_N2)
-        mrc_N2_p1 = fastVEC_prob(mrc2_p1, mrc_N2_p1)
-        mrc_N2_p2 = fastVEC_prob(mrc2_p2, mrc_N2_p2)
-        mrc_N2_p3 = fastVEC_prob(mrc2_p3, mrc_N2_p3)
-        mrc_N2_p4 = fastVEC_prob(mrc2_p4, mrc_N2_p4)
-                
-        #search_map_fft(mrc_N1, mrc_N2, TopN=topN, ang=angle_spacing, mode=modeVal, is_eval_mode=evalMode)
-        search_map_fft_prob(mrc_N1_p1, mrc_N1_p2, mrc_N1_p3, mrc_N1_p4, mrc_N1,mrc_N2, mrc_N2_p1, mrc_N2_p2, mrc_N2_p3, mrc_N2_p4,alpha=1,ang=angle_spacing, mode="VecProduct")
+        search_map_fft_prob(mrc_N1_p1, mrc_N1_p2, mrc_N1_p3, mrc_N1_p4, mrc_N1, mrc_N2, mrc_N2_p1, mrc_N2_p2, mrc_N2_p3,
+                            mrc_N2_p4, alpha=1, ang=angle_spacing, mode="VecProduct")
